68a571d502046cf671dc076684219cde1c3c6e55
Aongkon <jmaongkon733@gmail.com>
2026-05-13 18:29:24 +0600

    another commit

--- database/migrations/2026_05_12_105450_create_students_table.php
+++ database/migrations/2026_05_12_105450_create_students_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->string('gender');
-            $table->integer('score');
+            // $table->string('name');
+            // $table->string('email');
+            // $table->string('gender');
+            // $table->integer('score');
             $table->timestamps();
         });
     }
